00020610d9164a1d44b8ad494721fccc9c478f3d
Michael Sanger <michael@goodorbit.agency>
2023-03-22 14:58:32 -0700

    responsive progress

--- resources/views/template-custom.blade.php
+++ resources/views/template-custom.blade.php
@@ -2,16 +2,18 @@
   Template Name: Home Page
 --}}
 
-
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=KoHo:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
 <main>
   <section>
   <div id="particles-js" style="color:white;">
   <img class="logo fade-in" src="@asset('images/go-white-nobg.png')" />
   <h1 class="fade-in hero-h1">
-    Boutique Web Development Agency From Los Angeles
+    A Boutique Web Development Agency From Los Angeles
   </h1>
-  <button type="button" class="fade-in hello-button">VIEW THE WORK</button> 
+  <button type="button" class="fade-in hello-button">VIEW OUR WORK</button> 
   </div>
 </section>
 </main>
@@ -23,3 +25,15 @@
 
 
 
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
+      <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+    </div>
+  </div>
+</div>
